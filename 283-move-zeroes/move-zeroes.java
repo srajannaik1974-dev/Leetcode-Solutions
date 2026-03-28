9707e099579595292ddf9fbf3ff8165b249719de
srajannaik1974-dev <srajannaik1974@gmail.com>
2026-03-28 15:34:31 +0530

    Time: 2 ms (90.69%) | Memory: 47.7 MB (69.40%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int n=arr.length;
+        int j=0;
+         for(int i=0;i<n;i++)
+             {
+                if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+             }
+        }
+
+    }
+}
