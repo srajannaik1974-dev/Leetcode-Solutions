192ba8942da928bd327002080e0f6528fd443f5e
srajannaik1974-dev <srajannaik1974@gmail.com>
2026-04-04 12:56:01 +0530

    Time: 1 ms (99.98%) | Memory: 46.9 MB (55.68%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n=nums.length;
+        int XOR=0;
+        for(int i=0;i<n;i++){
+            XOR=XOR^nums[i];
+        }return XOR;
+    }
+}
