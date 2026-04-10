a6082d866948e6ea7c7ad42a5797b1f0fc5d2a35
srajannaik1974-dev <srajannaik1974@gmail.com>
2026-04-10 21:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (96.81%) - LeetSync

---
+++ 2630-alternating-digit-sum/alternating-digit-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        int sum=0;
+        int sign=1;
+        while(n>0){
+            int rem=n%10;
+             sum=sum+rem*sign;
+            sign*=-1;
+            n=n/10;
+        
+     
+        }if(sign==-1){
+            return sum;
+        }else{
+            return -sum;
+        }
+    }
+        }
+    
