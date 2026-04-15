3a911ef7574438e6f54164fc383bf432f5460ce4
srajannaik1974-dev <srajannaik1974@gmail.com>
2026-04-16 00:11:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (40.38%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -1,5 +1,15 @@
 class Solution {
     public int numberOfMatches(int n) {
-        return n-1;
-    }
+       int count=0;
+       while(n>1){
+        count+=n/2;
+        if(n%2==0){
+       n=n/2;
+   
+       }else{
+        n=(n-1)/2+1;
+  
+       }
+    }return count;
+}
 }
