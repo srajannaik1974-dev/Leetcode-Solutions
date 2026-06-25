a643ba5c844f30a14aaf128702163fb75024b46c
srajannaik1974-dev <srajannaik1974@gmail.com>
2026-06-25 11:51:02 +0530

    Time: 2 ms (91.98%) | Memory: 47 MB (99.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,18 +1,14 @@
 class Solution {
-    public void moveZeroes(int[] arr) {
-        int n=arr.length;
-      
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
         int j=0;
-       for( int i=0;i<n;i++){
-            if(arr[i]!=0){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
                 j++;
-
             }
         }
-        
-        
     }
 }
