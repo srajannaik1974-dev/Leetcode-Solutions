e5476a5ef3cedc502eee2e868be7f0d35aee1e5d
srajannaik1974-dev <srajannaik1974@gmail.com>
2026-07-21 07:27:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (18.46%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -5,13 +5,13 @@ class Solution {
         int high=n-2;
         if(n==1) return 0;
         if(nums[0]>nums[1])return 0;
-        if(nums[n-1]>nums[n-2]) return n-1;
+        if(nums[n-1]>nums[n-2])return n-1;
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
                 return mid;
             }else if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1]){
-                low=mid+1;
+                      low=mid+1;
             }else{
                 high=mid-1;
             }
