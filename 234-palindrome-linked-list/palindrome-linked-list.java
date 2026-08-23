712326a26d01c5e308ca3b65e9b95739e598aace
srajannaik1974-dev <srajannaik1974@gmail.com>
2026-08-23 10:28:57 +0530

    Time: 4 ms (67.01%) | Memory: 94.2 MB (90.41%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,22 +10,25 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> list=new ArrayList<>();
-        ListNode curr=head;
-        while(curr!=null){
-            list.add(curr.val);
-            curr=curr.next;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        int i=0;
-        int j=list.size()-1;
-        while(i<j){
-            if(list.get(i)==list.get(j)){
-                i++;
-                j--;
-            }else{
+        ListNode prev=null;
+        while(slow!=null){
+            ListNode next=slow.next;
+            slow.next=prev;
+            prev=slow;
+            slow=next;
+
+        }
+        while(prev!=null){
+            if(head.val!=prev.val){
                 return false;
-            }
+            }head=head.next;
+            prev=prev.next;
         }return true;
-        
     }
 }
