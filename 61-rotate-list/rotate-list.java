ccdf84830d1c0e0dc670425176a96a361fa4b422
srajannaik1974-dev <srajannaik1974@gmail.com>
2026-09-19 17:44:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (62.67%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -10,28 +10,35 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        int length=0;
+        ListNode curr=head;
         if(head==null || head.next==null){
             return head;
         }
-        ListNode curr1=head;
-        int len=0;
-        while(curr1.next!=null){
-            len++;
-            curr1=curr1.next;
+        while(curr!=null){
+            length++;
+            curr=curr.next;
         }
-        len=len+1;
-        k=k%len;
-        if(k==0)
-        return head;
-     
-        ListNode curr=head;
-        for(int i=1;i<len-k;i++){
+        curr=head;
+        ListNode dummy=new ListNode(0);
+        k=k%length;
+        if(k==0)return head;
+        
+        int limit=length-k;
+        ListNode left=head;
+        for(int i=1;i<limit;i++){
             curr=curr.next;
         }
         ListNode right=curr.next;
+        dummy.next=right;
         curr.next=null;
-        curr1.next=head;
-        return right;
+        curr=right;
+        while(curr!=null && curr.next!=null){
+            curr=curr.next;
+        }
+        curr.next=left;
+        
 
+        return dummy.next;
     }
 }
